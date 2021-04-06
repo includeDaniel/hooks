415d7bc3a1fe00aa2e01de86ab8d3617c25e42e5
Daniel Nunes <danielzcomputacao@gmail.com>
2021-04-06 13:39:28 -0300

    useEffect

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,15 +1,59 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useEffect, useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
-const UseEffect = (props) => {
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-        </div>
-    )
+function calcFatorial(num) {
+  const n = parseInt(num);
+  if (n < 0) return -1;
+  if (n === 0) return 1;
+  return calcFatorial(n - 1) * n;
 }
 
-export default UseEffect
+const UseEffect = (props) => {
+  const [number, setNumber] = useState(1);
+  const [fatorial, setFatorial] = useState(1);
+
+  useEffect(
+    function () {
+      setFatorial(calcFatorial(number));
+    },
+    [number]
+  );
+
+  useEffect(
+    function () {
+      if (fatorial > 1000000) {
+        alert("Fatorial acima de 1000000");
+      }
+    },
+    [fatorial]
+  );
+
+  return (
+    <div className="UseEffect">
+      <PageTitle
+        title="Hook UseEffect"
+        subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+      />
+      <SectionTitle title="Exercício #01" />
+      <div>
+        <div className="center">
+          <span className="text">Fatorial :</span>
+          <span className="text red">
+            {fatorial === -1 ? "Não existe" : fatorial}
+          </span>
+          <input
+            type="number"
+            className="input"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+          />
+        </div>
+      </div>
+      <SectionTitle title="Exercício #02" />
+      <div className="center"></div>
+    </div>
+  );
+};
+
+export default UseEffect;
