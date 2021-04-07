e0b3b634db5c75452131a27e907b5f7b7e6c6861
Daniel Nunes <danielzcomputacao@gmail.com>
2021-04-07 13:17:06 -0300

    useContext

--- hooks/src/views/App.jsx
+++ hooks/src/views/App.jsx
@@ -6,18 +6,21 @@ import Menu from "../components/layout/Menu";
 import Content from "../components/layout/Content";
 
 import DataContext, { data } from "../data/DataContext";
+import Store from "../data/Store";
 
 const App = (props) => {
   const [state, setState] = useState(data);
   return (
-    <DataContext.Provider value={{ state, setState }}>
-      <div className="App">
-        <Router>
-          <Menu />
-          <Content />
-        </Router>
-      </div>
-    </DataContext.Provider>
+    <Store>
+      <DataContext.Provider value={{ state, setState }}>
+        <div className="App">
+          <Router>
+            <Menu />
+            <Content />
+          </Router>
+        </div>
+      </DataContext.Provider>
+    </Store>
   );
 };
 
