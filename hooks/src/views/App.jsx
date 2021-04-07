a4023d97212323309dad40c25fdada30211b11a9
Daniel Nunes <danielzcomputacao@gmail.com>
2021-04-07 12:55:02 -0300

    useContext

--- hooks/src/views/App.jsx
+++ hooks/src/views/App.jsx
@@ -1,20 +1,24 @@
-import './App.css'
-import React from 'react'
-import { BrowserRouter as Router } from  'react-router-dom'
+import "./App.css";
+import React, { useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import Menu from '../components/layout/Menu'
-import Content from '../components/layout/Content'
+import Menu from "../components/layout/Menu";
+import Content from "../components/layout/Content";
 
-const App = props => {
+import DataContext, { data } from "../data/DataContext";
 
-    return (
-                <div className="App">
-                    <Router>
-                        <Menu />
-                        <Content />
-                    </Router>
-                </div>
-    )
-}
+const App = (props) => {
+  const [state, setState] = useState(data);
+  return (
+    <DataContext.Provider value={{ state, setState }}>
+      <div className="App">
+        <Router>
+          <Menu />
+          <Content />
+        </Router>
+      </div>
+    </DataContext.Provider>
+  );
+};
 
-export default App
+export default App;
--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useContext } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import DataContext from "../../data/DataContext";
 
 const UseContext = (props) => {
-    return (
-        <div className="UseContext">
-            <PageTitle
-                title="Hook UseContext"
-                subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
-            />
-        </div>
-    )
-}
+  const context = useContext(DataContext);
 
-export default UseContext
+  const addNumber = (delta) => {
+    context.setState({
+      ...context.state,
+      number: context.state.number + delta,
+    });
+  };
+
+  return (
+    <div className="UseContext">
+      <PageTitle
+        title="Hook UseContext"
+        subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
+      />
+
+      <div className="center">
+        <span className="text">{context.state.text}</span>
+        <span className="text">{context.state.number}</span>
+
+        <div>
+          <button className="btn" onClick={(e) => addNumber(-1)}>
+            -1
+          </button>
+          <button className="btn" onClick={(e) => addNumber(1)}>
+            +1
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UseContext;
